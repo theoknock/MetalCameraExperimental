fddf665d1d59fbeccfd22ed533a9d6041fb2e5e5
James Alan Bush <jamesalanbush1972@gmail.com>
2021-08-08 18:02:38 -0400

    Initial Commit

---
+++ MetalCameraExperimental/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  MetalCameraExperimental
+//
+//  Created by Xcode Developer on 8/8/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ MetalCameraExperimental/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  MetalCameraExperimental
+//
+//  Created by Xcode Developer on 8/8/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
